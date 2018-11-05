4aa40c1a235cdc44f8c7d75c2da07d0be8f3c275
viperzu <chiaraaltafunissa@gmail.com>
2018-11-05 22:16:42 +0700

    spammer

---
+++ SPAM.sh
@@ -0,0 +1,111 @@
+#!/system/xbin/bash
+clear
+sleep 1
+echo "\033[31;1m [*] Boom Sms [*]"
+sleep 1
+echo "\033[32;1m Author: Viperzu"
+sleep 1
+echo "\033[33;1m Contack: +13216666369"
+sleep 1
+echo "\033[35;1m Viperzu.cyber"
+sleep 1
+echo "\033[36;1m List Yang Tersedia :"
+echo "1. Tokopedia"
+echo "2. Telkomsel"
+echo "3. Matahari Mall"
+echo "4. PHD"
+echo "5. Jd.Id"
+echo "6. Email Bomber"
+echo "8. Credit"
+echo "0. Keluar"
+echo "\033[33;1m Pilih Angka:"
+read mrrm
+if [ $mrrm = 1 ] || [ $mrrm = 1 ]
+then
+clear
+echo "\033[36;1m"
+figlet "tokopedia"
+php 1.php
+fi
+
+if
+[ $mrrm = 2 ] || [ $mrrm = 2 ]
+then
+clear
+echo "\033[36;1m"
+toilet "T-Sel"
+php 2.php
+fi
+
+if [ $mrrm = 3 ] || [ $mrrm = 3 ]
+then
+clear
+echo "\033[36;1m"
+figlet "Mthr Mall"
+php 3.php
+fi
+
+
+if [ $mrrm = 4 ] || [ $mrrm = 4 ]
+then
+clear
+toilet -f mono12 -F gay "PHD"
+echo "\033[36;1m"
+php 4.php
+fi
+
+if
+[ $mrrm = 5 ] || [ $mrrm = 5 ]
+then
+clear
+toilet -f mono12 -F gay "Jd.Id"
+echo "\033[36;1m"
+php 5.php
+fi
+
+if
+[ $mrrm = 6 ] || [ $mrrm = 6 ]
+then
+clear
+toilet -f standard -F gay "Email Bomber"
+echo "\033[36;1m"
+python2 6.py
+fi
+
+if
+[ $mrrm = 8 ] || [ $mrrm = 8 ]
+then
+clear
+echo "\033[31;1mNama Team: Viperzu"
+sleep 1
+echo "\033[33;1mVersi: v2"
+sleep 1
+echo "\033[32;1mNama Pembuat: Muh.Jafar"
+sleep 1
+echo "\033[33;1m Thanks To:"
+echo "\033[36;1m"
+sleep 1
+echo "Handika Pratama"
+sleep 1
+echo "まやちゃん"
+sleep 1
+echo "Baby Cyber Team"
+sleep 1
+echo "SBG-Team"
+sleep 1
+echo "Ponorogo Defacer Team"
+sleep 1
+echo "And All Member Viperzu"
+sleep 3
+sh SPAM.sh
+fi
+
+if
+[ $mrrm = 0 ] || [ $mrrm = 0 ]
+then
+echo "\033[31;1m Keluar"
+sleep 1
+echo "\033[32;1m Sampai berjumpa lagi :)"
+sleep 1
+fi
+
